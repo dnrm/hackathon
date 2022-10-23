d4f273768a31c06375c0278a753bbc9dd925e0e8
salogod <102626192+salogod@users.noreply.github.com>
2022-10-23 09:27:14 -0500

    shop

--- lib/pages/shop.dart
+++ lib/pages/shop.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:hackathon/pages/shop_container.dart';
 
 class Shop extends StatefulWidget {
-  const Shop({super.key});
+  Shop({Key? key}) : super(key: key);
 
   @override
   State<Shop> createState() => _ShopState();
@@ -10,22 +13,68 @@ class Shop extends StatefulWidget {
 class _ShopState extends State<Shop> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          Text(
-            'Shop',
-            style: TextStyle(
-              fontWeight: FontWeight.w800,
-              fontSize: 28,
-              color: Colors.white,
-              letterSpacing: -1.5,
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          toolbarHeight: 100,
+          leading: IconButton(
+              onPressed: onTap(),
+              icon: const Icon(
+                Icons.arrow_back_ios,
+                color: Colors.white,
+              )),
+          actions: const <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 33,
+              child: CircleAvatar(
+                radius: 29,
+                backgroundImage: AssetImage("images/toby.png"),
+              ),
             ),
+          ],
+        ),
+        body: Padding(
+          padding:
+              const EdgeInsets.only(top: 20, right: 10, left: 10, bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Shop",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 30),
+              Container(
+                height: 1,
+                decoration: const BoxDecoration(
+                  color: Colors.white38,
+                ),
+              ),
+              const SizedBox(height: 40),
+              Row(
+                children: [
+                  ShopContainer(cantidad: "x50", costo: "\$" + "100"),
+                  const SizedBox(width: 40),
+                  ShopContainer(cantidad: "x100", costo: "\$" + "150")
+                ],
+              ),
+              SizedBox(height: 40),
+              Row(
+                children: [
+                  ShopContainer(cantidad: "x300", costo: "\$" + "200"),
+                  const SizedBox(width: 40),
+                  ShopContainer(cantidad: "x1000", costo: "\$" + "700")
+                ],
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
+
+  onTap() {}
 }
